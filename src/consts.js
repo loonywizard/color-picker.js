873ed7e1fd9badff86ee742d78896fad9bb5543a
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-10-28 11:26:19 +0300

    Add consts.js file that contains sizes of pickers and ranges for HSB color values

---
+++ src/consts.js
@@ -0,0 +1,27 @@
+/**
+ * Width and height of usual picker, where there'is only one coordinate
+ */
+export const PICKER_WIDTH = 250;
+export const PICKER_HEIGHT = 14;
+
+/**
+ * Height of a special picker, where there's two coordinates:
+ * saturation and brightness
+ */
+export const SATURATION_BRIGHTNESS_PICKER_HEIGHT = 150;
+
+/**
+ * Ranges in which specific color value can be
+ */
+export const HUE_RANGE = {
+  MIN: 0,
+  MAX: 360,
+};
+export const SATURATION_RANGE = {
+  MIN: 0,
+  MAX: 100,
+};
+export const BRIGHTNESS_RANGE = {
+  MIN: 0,
+  MAX: 100,
+};
